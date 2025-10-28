b684bf9406f449a4943d728d3d5fd612cae4c5ab
CloggedOsprey4 <chasekoster2002@gmail.com>
2025-10-28 17:09:31 -0500

    HTML Star tek UI -Chase

--- src/main/windows.js
+++ src/main/windows.js
@@ -9,11 +9,11 @@ export async function createMainWindow() {
     width: 1200,
     height: 800,
     show: true,
-    frame: false,            // overlay vibe (frameless)
+    frame: true,             // borderless vs windowed (currently windowed for debug purposes)
     alwaysOnTop: true,       // stays above other windows
     transparent: false,      // set true later if you want HUD
     webPreferences: {
-      preload: path.join(__dirname, '../preload/index.js'),
+      preload: path.join(__dirname, '../preload/home.js'),
       nodeIntegration: false,
       contextIsolation: true,
       sandbox: true
@@ -21,7 +21,8 @@ export async function createMainWindow() {
   });
 
   // Load the Home page
-  await win.loadFile(path.join(__dirname, '../renderer/index.html'));
+  //await win.loadFile(path.join(__dirname, '../renderer/startup.gif'))
+  await win.loadFile(path.join(__dirname, '../renderer/home.html'));
 
   // Optional: devtools while developing
   // win.webContents.openDevTools({ mode: 'detach' });
